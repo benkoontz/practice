c61a854e27e891397e02503ee61589e81387d90f
abc <abc@example.com>
2020-01-01 13:42:42 -0700

    made changes to fish

--- fish.java
+++ fish.java
@@ -1,46 +1,30 @@
 
 import java.util.Scanner;
+
+// inherit the fields and methods from the apples class
 public class fish extends apples {
-
- int x;
-
- 
- public fish() {
-	 x = 5;
- }
- 
 
 
 	public static void main(String[] args) {
 		
-	//	Scanner sc = new Scanner(System.in);
-		
-	//	String hello = sc.nextLine();
-		
-	//	System.out.println("output " + hello);
-		//hello2();
-
+		// calling two score methods using static, the first name is always 50 and the second is a number between 50 and 0
 		System.out.println("the score of the halo game was " + Score1(50,50) + " to " + Score2(50,0));
 		
-		String array [] = {"apple","banna","fishy"};
-		
+		// create an apples object to use
 		apples obj = new apples();
 		
-	//	System.out.println(obj.fname);
-		
-		int y = 2;
-		System.out.println(y);
-		
-		//apples obj = new apples();
-		
-		//System.out.println(obj.ben);
-		
-		obj.carPayment();
-		
-		System.out.println(Score1(50,40));
+		// call the carPayment method
+		obj.carPayment(); 
 		
 		System.out.println("your salary is " + obj.netIncome(obj.salary));
+		
+		// encapsulation
+		apples nameObj = new apples();
+		nameObj.setName("ben");
+		System.out.println("Name: " + nameObj.getName());
 		}
+	
+
 		
 
 	}
