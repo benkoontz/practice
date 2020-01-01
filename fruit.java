6efa998baeba590bb1101e4512c5ddfa16bb7edc
abc <abc@example.com>
2020-01-01 13:06:05 -0700

    making changes to fruit

--- fruit.java
+++ fruit.java
@@ -2,9 +2,12 @@
 
 public abstract class fruit {
 	
-	public abstract int netIncome(int salary);
+	// abstract method, these must be overwritten in another class
+	public abstract int netIncome(int salary); 
 	public abstract void carPayment();
 	public abstract void housePayment();
+	
+	// attributes initialized by a constructor in the apples class
 	public int salary;
 	public int carPrice;
 	public int housePrice;
