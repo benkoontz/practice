a1016096d5713255cf96544f6c86d6a3f044318e
abc <abc@example.com>
2020-01-01 14:53:39 -0700

    making changes to fruit

--- fruit.java
+++ fruit.java
@@ -1,5 +1,5 @@
 
-
+// Create an abstract class. Create an abstract class when you want to create a base for another class.
 public abstract class fruit extends Animal {
 	
 	// abstract method, these must be overwritten in another class
