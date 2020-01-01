433988cc1014e02dfd90bfc041e3bc24f2946672
abc <abc@example.com>
2020-01-01 13:42:27 -0700

    made changes to apples

--- apples.java
+++ apples.java
@@ -54,4 +54,16 @@ public class apples extends fruit{
 		
 		return randomNum;
 	}
+	
+	  // encapsulation:
+	
+	  // Getter
+	  public String getName() {
+	    return name;
+	  }
+
+	  // Setter
+	  public void setName(String newName) {
+	    this.name = newName;
+	  }
 }
