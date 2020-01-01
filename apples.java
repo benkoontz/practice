a3c3e686c91195259c9ad123e8893d5255073838
abc <abc@example.com>
2020-01-01 13:00:36 -0700

    making changes to apples

--- apples.java
+++ apples.java
@@ -4,9 +4,18 @@ import java.util.Random;
 
 public class apples extends fruit{
 	
-	// This method is used to calculate a salary 
-	public  void salary() {
-		System.out.println("your yearly salary is $52,000");
+	// This method is used to calculate a net income
+	public  int netIncome(int salary) {
+		
+		// subtract 1000 each week for expenses
+		for(int i = 0; i < 52; i++) {
+			salary -= 1000;
+		}  //the salary is no 52000 less original amount: 80000 - 52000 = 280000
+		
+		// calculate a random salary between 56000 and 280000
+		salary = Score1(salary*2, salary);
+		
+		return salary;
 	}
 	
 	// This method is used to calculate a car payment
