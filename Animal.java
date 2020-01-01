a6d8309e88663ef7f3825d289e1cd3bb2b9bfe41
abc <abc@example.com>
2020-01-01 14:31:31 -0700

    crreated Animal

---
+++ Animal.java
@@ -0,0 +1,11 @@
+
+
+// This class was created to represent polymorphism:
+//  It is useful for code reusability: reuse attributes and methods of an existing class when you create a new class.
+public class Animal {
+
+	public void animalSound() {
+		System.out.println("animal sound");
+	}
+	
+}
